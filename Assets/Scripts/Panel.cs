beb32385c88173cdd7852003ac8f86ffa92dabf6
Moroka8 <moroka8@mail.ru>
2024-04-25 10:58:26 +0700

    content attach fix
    I realized that content can be linked right inside prefab)

--- Assets/Scripts/Panel.cs
+++ Assets/Scripts/Panel.cs
@@ -4,16 +4,14 @@ using UnityEngine;
 
 public class Panel : MonoBehaviour
 {
-    private Transform content;
+    public Transform content;
 
     public void AddObjects(List<GameObject> objs) {
-        Transform content = gameObject.transform.Find("Content");
         foreach (GameObject obj in objs)
         {
             GameObject block = (GameObject)Instantiate(obj, content);
         }
     }
-
 
     public void ClosePanel() {
         Destroy(gameObject);
